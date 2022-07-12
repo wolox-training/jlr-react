51762c3e41c4faa7b7e19cf0f27ad4806729698c
joseleonardorojas <jose.leonardo.rojas@accenture.com>
2022-07-12 16:08:09 -0500

    Add search bar

--- src/components/BookList/index.tsx
+++ src/components/BookList/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -10,6 +10,7 @@ import Loading from '../Spinner/components/loading';
 import { IBook } from '../../utils/types';
 
 import ImgAdd from './assets/Add.png';
+import ImgSearch from './assets/Search.png';
 import styles from './styles.module.scss';
 
 function NavBar() {
@@ -24,24 +25,39 @@ function NavBar() {
     dispatch(actionCreators.addBook({ id, author, title }));
   };
 
+  const [search, setSearch] = useState('');
+
+  const filteredBook = books.filter(book => book.title.toLowerCase().includes(search.toLowerCase()));
+
   return (
     <div className={styles.container}>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        books.map((book: IBook) => (
-          <div key={book.id} className={styles.cardBook}>
-            <Link to={`/books/${book.id}`}>
-              <img className={styles.imgBook} src={book.image_url} alt={t('Images:bookCover')} />
-              <span className={styles.titleBook}>{book.title}</span>
-              <span className={styles.authorBook}>{book.author}</span>
-            </Link>
-            <button type="button" onClick={() => addBook(book)}>
-              <img className={styles.imgAdd} src={ImgAdd} alt={t('BookList:imgAdd')} />
-            </button>
-          </div>
-        ))
-      )}
+      <div className={styles.inputSearch}>
+        <input
+          type="text"
+          placeholder="Buscá por título del libro..."
+          className={styles.searchBook}
+          onChange={e => setSearch(e.target.value)}
+        />
+        <img src={ImgSearch} className={styles.imgSearch} />
+      </div>
+      <div className={styles.containerBook}>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          filteredBook.map((book: IBook) => (
+            <div key={book.id} className={styles.cardBook}>
+              <Link to={`/books/${book.id}`}>
+                <img className={styles.imgBook} src={book.image_url} alt={t('Images:bookCover')} />
+                <span className={styles.titleBook}>{book.title}</span>
+                <span className={styles.authorBook}>{book.author}</span>
+              </Link>
+              <button type="button" onClick={() => addBook(book)}>
+                <img className={styles.imgAdd} src={ImgAdd} alt={t('BookList:imgAdd')} />
+              </button>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
